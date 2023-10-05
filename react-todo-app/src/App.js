6f2b6a2c83c8c956c42ef47528aa3bc36183e838
Rui Calvario <66223767+rui-calvario@users.noreply.github.com>
2023-10-05 14:07:29 +0000

    Set Input

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
-	return <div className='todo-app'>Todo App</div>;
+	return (
+		<div className='todo-app'>
+			<TodoList />
+		</div>
+	);
 }
 
 export default App;
